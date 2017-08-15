2bb3756ab5a2fb801c36bf45d3b5dc6aff3ebb45
SID-PC\SiD <siddharth.nagrajann@gmail.com>
2017-08-15 18:51:56 +0530

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose}=  require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -35,6 +36,24 @@ app.get('/todos',(req, res)=>{
     })
 });
 
+app.get('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send({
+            error : "ObjectID is invalid"
+        });
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send({});
+        }
+        res.status(200).send({todo});
+    },(e)=>{
+        res.status(404).send(e);
+    });
+});
 
 app.listen(3000, ()=>{
     console.log('started on port 3000');
