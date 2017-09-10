e33c8fddc817dec330abdf99061cd39901fd24bb
SID-PC\SiD <siddharth.nagrajann@gmail.com>
2017-09-10 16:50:35 +0530

    test the DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -60,18 +60,18 @@ app.delete('/todos/:id',(req,res)=>{
     var id = req.params.id;
 
     if(!ObjectID.isValid(id)){
-        return res.status(404).send();
+        return res.status(404).send({});
     }
 
     Todo.findByIdAndRemove(id).then((todo)=>{
         if(!todo){
-            return res.status(404).send();
+            return res.status(404).send({});
         }
 
-        res.send(todo);
+        res.send({todo});
 
     }).catch((e)=>{
-        res.status(400).send();
+        res.status(400).send({});
 
     })
 
