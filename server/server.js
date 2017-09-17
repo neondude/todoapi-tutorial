1763825f6ce4bc7fffd0174da1cdc46d48f26102
SID-PC\SiD <siddharth.nagrajann@gmail.com>
2017-09-17 10:59:39 +0530

    Add tests for PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -86,7 +86,7 @@ app.patch('/todos/:id',(req,res)=>{
     if(!ObjectID.isValid(id)){
         return res.status(404).send({});
     }
-    
+
 
     if(_.isBoolean(body.completed) && body.completed){
         body.completedAt = new Date().getTime();
