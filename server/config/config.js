4b4fe4b5bab91a61019d5eb840814ad342095752
SID-PC\SiD <siddharth.nagrajann@gmail.com>
2017-09-17 12:08:58 +0530

    fixed heroku port error

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,5 @@ if(env == 'development'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }else if(env == 'production'){
-    process.env.PORT = 3000;
     process.env.MONGODB_URI = process.env.PROD_MONGODB;
 }
